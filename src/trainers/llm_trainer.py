e04e6165041e5ee728615b281fdd9659e2256fc7
Adam Klein <adam3207@gmail.com>
2025-07-22 07:43:10 +0000

    cvbnm,

--- src/trainers/llm_trainer.py
+++ src/trainers/llm_trainer.py
@@ -6,6 +6,7 @@ from trainers.base_trainer import BaseTrainer
 class LLMTrainer(BaseTrainer):
 
     def forward(self, input_ids, mask):
+        print(f" ==== {input_ids.shape} ==== ", flush=True)
 
         _logits, loss = self.model(
             input_ids=input_ids,

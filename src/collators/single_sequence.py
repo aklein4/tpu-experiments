3b7dc8a7b4468489f70a5e44ccab9aedc80fe46e
Adam Klein <adam3207@gmail.com>
2025-07-22 07:24:19 +0000

    dsfjvb

--- src/collators/single_sequence.py
+++ src/collators/single_sequence.py
@@ -27,6 +27,8 @@ class SingleSequenceCollator:
             in_ids = torch.tensor(x["input_ids"]).long()
             out_ids = torch.tensor(x["output_ids"]).long()
 
+            print(in_ids.shape, out_ids.shape, flush=True)
+
             input_ids.append(torch.cat([in_ids, out_ids], dim=-1))
         
         # pad to length

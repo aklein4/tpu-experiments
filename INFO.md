b2a694217b6a2dcb2de9060ca47e0004050d6e75
Adam Klein <adam3207@gmail.com>
2025-07-23 16:16:16 -0700

    configs for testing

--- INFO.md
+++ INFO.md
@@ -4,5 +4,6 @@
 Based on commit `643cf82c545e315a2b8f7a20f586811308400f87`
 
 ### torch/xla
+
 Use version `2.9.0` from date `20250709`
 
---
+++ setup_vm_new.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+: '
+Setup a TPU VM to use the repo.
+ - MUST RUN WITH dot (.) command to set the environment variables in the current shell.
+
+Arguments:
+    $1: Huggingface token
+    $2: wandb token
+
+Example:
+    . setup_vm.sh <HF_TOKEN> <WANDB_TOKEN>
+'
+
+# # upgrade and update pip
+pip install --upgrade pip
+pip install --upgrade setuptools
+
+# install torch
+pip3 install torch==2.9.0.dev20250423+cpu --index-url https://download.pytorch.org/whl/nightly/cpu
+
+# install torch_xla for TPU VM
+# Edit `cp310-cp310` to fit your desired Python version as needed
+pip install 'torch_xla[tpu] @ https://storage.googleapis.com/pytorch-xla-releases/wheels/tpuvm/torch_xla-2.9.0.dev-cp310-cp310-linux_x86_64.whl' \
+  -f https://storage.googleapis.com/libtpu-wheels/index.html
+
+# update path(?)
+export PATH="/home/$USER/.local/bin:$PATH"
+
+# install extras
+pip install transformers datasets matplotlib huggingface_hub hydra-core omegaconf
+pip install --upgrade datasets
+pip install wandb
+
+# login to huggingface
+# huggingface-cli login --token $1 --add-to-git-credential
+
+# login to wandb
+python -m wandb login $2

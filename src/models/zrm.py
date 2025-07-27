12b1d14090d2647403386a411a9df6e3facbcf7c
Adam Klein <adam3207@gmail.com>
2025-07-27 00:46:58 -0700

    no alpha scaling

--- src/models/zrm.py
+++ src/models/zrm.py
@@ -213,7 +213,7 @@ class ZRMModel(BaseXLAModel):
 
         # get the real alpha value
         alpha = F.softplus(self.log_alpha.mean()) / np.log(2.0)
-        alpha = alpha * np.sqrt(np.log(self.vocab_size) / self.z_size)
+        # alpha = alpha * np.sqrt(np.log(self.vocab_size) / self.z_size)
         # alpha = np.sqrt(np.log(self.vocab_size) / self.z_size)
 
         # get reusable components

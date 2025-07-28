2ef2886252326bf1214b0dcf49e145cb4d53bc3e
Adam Klein <adam3207@gmail.com>
2025-07-28 12:14:25 -0700

    sharding

--- src/torchprime/torch_xla_models/model_rewriting/sharding_initialization.py
+++ src/torchprime/torch_xla_models/model_rewriting/sharding_initialization.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 
 def setup_sharding_and_mesh(
-  model: nn.Module, config: DictConfig
+  model: nn.Module, config: DictConfig, shard_model=True
 ) -> tuple[nn.Module, xs.ShardingSpec, bool]:
   """Sets up XLA mesh topology and applies SPMD sharding annotations to the model.
 
@@ -64,6 +64,7 @@ def setup_sharding_and_mesh(
   # the training across devices following the SPMD paradigm.
   sharding_config = OmegaConf.to_container(config.model.sharding, resolve=True)
   assert isinstance(sharding_config, dict)
-  model = shard_torch_xla_model_from_config(model, config=sharding_config)
+  if shard_model:
+    model = shard_torch_xla_model_from_config(model, config=sharding_config)
 
   return model, input_sharding_spec, minibatch
--- src/trainers/base_trainer.py
+++ src/trainers/base_trainer.py
@@ -99,6 +99,11 @@ class BaseTrainer:
         # Add `xp.Trace` to linear layers in the module tree (just for profiling?).
         model = auto_trace(model)
 
+        # Setup mesh
+        model, self.input_sharding_spec, self.minibatch = setup_sharding_and_mesh(
+            model, config, shard_model=False
+        )
+
         # Mark pure modules in the model for optimization.
         model = mark_pure_modules(model, config)
 
@@ -231,7 +236,7 @@ class BaseTrainer:
             drop_last=True,
         )
         loader = pl.MpDeviceLoader(
-            dataloader, self.device, # input_sharding=self.input_sharding_spec
+            dataloader, self.device, input_sharding=self.input_sharding_spec
         )
         return loader
     

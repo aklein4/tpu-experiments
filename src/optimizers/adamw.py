b84bfe5508a8ac97d204b203c052bcb33f734fb5
Adam Klein <adam3207@gmail.com>
2025-07-28 05:01:07 -0700

    different nan_to_num thing

--- src/optimizers/adamw.py
+++ src/optimizers/adamw.py
@@ -77,7 +77,8 @@ class AdamW(Optimizer):
                     raise RuntimeError("AdamW does not support sparse gradients.")
 
                 # handle nan gradients
-                grad = torch.nan_to_num(grad, nan=0.0, posinf=0.0, neginf=0.0)
+                # grad = torch.nan_to_num(grad, nan=0.0, posinf=0.0, neginf=0.0)
+                grad = torch.where(~torch.isfinite(grad), torch.zeros_like(grad), grad)
 
                 state = self.state[p]
 

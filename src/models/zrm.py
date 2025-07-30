8c86eedab78d7fce6a284a5aff5711338e1d5112
Adam Klein <adam3207@gmail.com>
2025-07-30 08:59:35 -0700

    correct alpha scale with 2

--- src/models/zrm.py
+++ src/models/zrm.py
@@ -225,7 +225,7 @@ class ZRMModel(nn.Module):
         alpha = F.softplus(
             self.log_alpha.mean() # * self.lr_scaler
         ) / np.log(2.0)
-        alpha = alpha * np.sqrt(np.log(self.vocab_size) / self.z_size)
+        alpha = alpha * np.sqrt(2 * np.log(self.vocab_size) / self.z_size)
         # alpha = np.sqrt(np.log(self.vocab_size) / self.z_size)
 
         z_scale = 1 / torch.sqrt(alpha**2 + noise_scale**2)

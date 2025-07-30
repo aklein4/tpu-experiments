0ad52e12cd048861f2440b91fe9c2ab012ef3c72
Adam Klein <adam3207@gmail.com>
2025-07-29 17:11:24 -0700

    move to cpu in sharding

--- src/trainers/base_trainer.py
+++ src/trainers/base_trainer.py
@@ -257,7 +257,7 @@ class BaseTrainer:
         # move the model to CPU for saving
         logger.info("Moving model to CPU for checkpoint saving...")
         state = {
-            k: xs.clear_sharding(v)
+            k: xs.clear_sharding(v).cpu()
             for k, v in self.model.state_dict().items()
         }
         xm.mark_step()

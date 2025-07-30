70154460f488c0edf2ff8d59d4ed2ecf8aba126e
Adam Klein <adam3207@gmail.com>
2025-07-29 17:39:12 -0700

    torch save

--- src/trainers/base_trainer.py
+++ src/trainers/base_trainer.py
@@ -274,7 +274,7 @@ class BaseTrainer:
 
             logger.info(f"Saving checkpoint to {save_path}")
             print({k: v.device for k, v in state.items()}, flush=True)
-            xm.save(state, os.path.join(save_path, "model.pt"))
+            torch.save(state, os.path.join(save_path, "model.pt"))
             with open(os.path.join(save_path, "config.json"), "w") as f:
                 json.dump(OmegaConf.to_container(self.config, resolve=True), f, indent=4)
             logger.info(f"Saved checkpoint to {save_path}")
